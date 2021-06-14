d16c625c172643aed92ea8da679dcd8a7d7c3ee2
sshiff@gmail.com <2afa246bbc544dadc57fc83a681bfe9978df9169>
2021-06-13 21:45:50 -0400

    initial commit, lots of new code

--- api.js
+++ api.js
@@ -1,74 +1,95 @@
 
 
 $(document).ready(function(){
+
+	const TOSCA_SERVER_PROTOCOL = 'http://';
+	const TOSCA_SERVER = 'localhost';
+	const TOSCA_SERVER_PORT = ':90'; //":90"
+	const ENDPOINT = '/Rest/ToscaCommander';
+	var TOSCA_ENDPOINT = TOSCA_SERVER_PROTOCOL + TOSCA_SERVER + TOSCA_SERVER_PORT + ENDPOINT;
+
 	// Event listeners
-	$("#start").click(function(){doAPI();});
+	$("#start").click(function(){doAPI(TOSCA_ENDPOINT + "/GetWorkspaces", "/GetWorkspaces", "", "");});
+
+
 /*
-	$("#nextenterproductdata").click(function(){checkAddPerson();});
-	$("#selectpriceoption").click(function(){checkAddPerson();});
 	$("#lastname").change(function(){checkGetPerson();checkRating();});
-	$("#firstname").change(function(){checkGetPerson();checkRating();});
-	$("#birthdate").change(function(){checkRating();});
-	$("#engineperformance").change(function(){checkRating();});
 	$("#sendemail").attr("id","sendemailCHANGED"); // this disables the PDF-send.
-	$("#sendemailCHANGED").click(function(){checkSendQuote();});
-	$("#contactAgent").click(function(){checkContactAgentForm();});
-	$("#resetForm").click(function(){resetContactForm();});
 
 	$("div.fullwidth-block .container").append("<div id='soaplog_div'><b>LOG:</b><hr/><div id='soaplog'></div></div>");
 	$(".main-content .breadcrumbs .container").prepend("<button id='toggle-log' name='Toggle Log'><i class='fa fa-toggle-off' /> Log</button>");
 	$("#numberofseatsmotorcycle").empty();
 	$("#numberofseats").empty();
 */
-});
 
- function doAPI() {
-	try {
-		//const response = await axios.get('http://localhost:90/Rest/ToscaCommander/GetWorkspaces');
-		//console.log(response);
-		var url = "http://a7e8df33060e.ngrok.io/Rest/ToscaCommander/GetWorkspaces";
+	async function doAPI(url, task, user, pass) {
+		try {
+			console.log("url: " + url +
+			"\ntask: " + task +
+			"\nuser: " + user +
+			"\npass: " + pass
+		);
 
-		const response =  fetch(url, {
-			method: 'GET',
-			mode: 'no-cors',
-			headers: {
-				'Accept': 'application/json',
-				'Content-Type': 'application/json'
-			}
-		 })
-			.then(validateResponse)
-			.then(readResponseAsJSON);
-			/*
-			.then(logResult)
-			.catch(logError);
-			*/
+		var conf = null;
+		if ((user != "") && pass != "") {
+			conf = {
+				auth: {
+					username: 'Tosca',
+					password: 'tosca'
+				}
+			};
+		}
 
-	} catch (error) {
-		console.error(error);
+		const serverResponse = await axios.get(url, conf)
+			.then((response) => {
+			validateResponse(response);
+			//writeResults(response, task);
+		});
+console.log(serverResponse)
+} catch (error) {;
+			console.error(error);
+		}
 	}
-}
 
-function validateResponse(response) {
-	console.log("response.ok = "+response.ok)
-  // if (!response.ok) {
-  //   throw Error(response.statusText);
-  // }
-  return response;
-}
+	function validateResponse(response) {
+		console.log("response.status = " + response.status)
+	   if (response.status != 200) {
+	     throw Error(response.statusText);
+	   }
+	  return response;
+	}
 
-function readResponseAsJSON(response) {
-	//console.log.arguments(response);
-	console.log(response);
-	console.log(response.headers);
-	//console.log(response.__proto__.json.arguments);
+	function writeResults(result, uri) {
+		console.log(result.data);
+		switch (uri) {
+			case "/GetWorkspaces":
+				writeWorkspaces(result.data);
+				break;
+			case "\projectid":
+				return "sss;";
+				break;
+			default:
 
-	//return response.json();
-}
+		}
+	}
 
-function logResult(result) {
-  console.log(result);
-}
+	function writeWorkspaces(list) {
+		$("#canvas").text("");
+		var out = "";
+    var i;
+    for(i = 0; i < list.length; i++) {
+			getProgectID(encodeURI(TOSCA_ENDPOINT + list[i] + "\\projectid"))
+			//$("#canvas").append("<LI>" + TOSCA_ENDPOINT + list[i] + "\\projectid");
+			//$("#canvas").append('<a href="' + list[i] + '">' + list[i] + '</a><br>');
+    }
+	}
 
-function logError(error) {
-  console.log('Looks like there was a problem: \n', error);
-}
+	function getProgectID(url) {
+		doAPI(url, "\projectid", "Tosca", "tosca")
+	}
+
+	function logError(error) {
+	  console.log('Looks like there was a problem: \n', error);
+	}
+
+}); // end of $(document).ready(function()
